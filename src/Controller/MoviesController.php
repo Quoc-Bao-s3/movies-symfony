faab6c51704956fd790b291af8aaca1fbb77bd3d
Quoc-Bao-213 <taquocbao.it.99@gmail.com>
2022-11-21 16:56:37 +0700

    Read Blog items

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -12,15 +12,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private $em;
+    // private $em;
+    private $movieRepository;
 
-    public function __construct(EntityManagerInterface $em)
+    // public function __construct(EntityManagerInterface $em)
+    // {
+    //     $this->em = $em;
+    // }
+
+    public function __construct(MovieRepository $movieRepository)
     {
-        $this->em = $em;
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name: 'movies')]
-    public function index(EntityManagerInterface $em): Response
+    #[Route('/movies', methods: ['GET'], name: 'movies')]
+    public function index(): Response
     {
         // findAll() - SELECT * FROM movies
         // find() - SELECT * FROM movies WHERE id = ?
@@ -32,7 +38,20 @@ class MoviesController extends AbstractController
         // $movies = $repository->getClassName();
         
         // dd($movies);
+        $movies = $this->movieRepository->findAll();
 
-        return $this->render('index.html.twig');
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
+        ]);
+    }
+
+    #[Route('/movies/{id}', methods: ['GET'], name: 'movie')]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie
+        ]);
     }
 }
